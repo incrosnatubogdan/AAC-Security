095873b394362341dedc411011a2dad431c7a204
Incrosnatu Bogdan <incrosnatubogdan@gmail.com>
2024-03-20 21:06:34 +0100

    built

--- vite.config.js
+++ vite.config.js
@@ -5,12 +5,15 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "src"),
-      vue: "vue/dist/vue.esm-bundler.js",
+    plugins: [vue()],
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "src"),
+            vue: "vue/dist/vue.esm-bundler.js",
+        },
     },
-  },
-  base: "./",
+    base: "./",
+    build: {
+        outDir: './docs'
+    }
 });
