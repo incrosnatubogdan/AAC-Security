5b4c91639b6dc7a2a76a0df46d3d9beffa786ddf
Incrosnatu Bogdan <incrosnatubogdan@gmail.com>
2024-12-18 15:54:25 +0200

    preload and serve as img

--- src/views/pages/HomePage.vue
+++ src/views/pages/HomePage.vue
@@ -16,6 +16,7 @@ export default {
   data() {
     return {
       isFirstSlide: true,
+      loadedHeader: null,
     };
   },
 
@@ -23,6 +24,9 @@ export default {
     const body = document.getElementsByTagName("body")[0];
     body.classList.add("bg-gray-200");
     window.location.href = document.URL.replace(/#.*$/, "") + "#landing-page";
+    this.loadedHeader = new Image();
+    this.loadedHeader.src =
+      "https://fomantic-ui.com/images/wireframe/image.png";
   },
 
   beforeUnmount() {
@@ -39,8 +43,14 @@ export default {
     <Header>
       <div
         class="page-header min-vh-100 animated fade-in"
-        :style="`background-image: url(${vueMkHeader}); box-shadow:inset 0 0 0 2000px rgb(0 0 0 / 40%)`"
+        :style="`box-shadow:inset 0 0 0 2000px rgb(0 0 0 / 40%)`"
       >
+        <img
+          class="header-img"
+          :src="vueMkHeader"
+          alt="Beyond Safety: Total Peace of Mind AAP Security London"
+          fetchpriority="high"
+        />
         <div class="container">
           <div class="row">
             <div class="col-lg-7 text-center mx-auto position-relative">
@@ -63,6 +73,12 @@ export default {
     data-anchor="landing-page2"
     :style="`background-image: url(${home_bg_1}); box-shadow:inset 0 0 0 2000px rgb(0 0 0 / 40%)`"
   >
+    <img
+      class="header-img"
+      :src="home_bg_1"
+      alt="Personal Security London with Corportate Security AAP Security London"
+      fetchpriority="eager"
+    />
     <div class="container">
       <div class="row">
         <div class="col-lg-12 text-center">
@@ -110,7 +126,11 @@ export default {
     </div>
     <div class="carousel-inner">
       <div class="carousel-item active">
-        <img :src="home_bg_3" class="d-block min-vh-100" alt="..." />
+        <img
+          :src="home_bg_3"
+          class="d-block min-vh-100"
+          alt="Commitment to Excellence"
+        />
         <div class="carousel-caption d-block">
           <h5 class="c-white">Commitment to Excellence</h5>
           <p class="lead">
@@ -121,7 +141,11 @@ export default {
         </div>
       </div>
       <div class="carousel-item">
-        <img :src="home_bg_3" class="d-block min-vh-100" alt="..." />
+        <img
+          :src="home_bg_3"
+          class="d-block min-vh-100"
+          alt="Proactive Approach"
+        />
         <div class="carousel-caption d-block">
           <h5 class="c-white">Proactive Approach</h5>
           <p class="lead">
@@ -132,7 +156,11 @@ export default {
         </div>
       </div>
       <div class="carousel-item">
-        <img :src="home_bg_3" class="d-block min-vh-100" alt="..." />
+        <img
+          :src="home_bg_3"
+          class="d-block min-vh-100"
+          alt="Comprehensive Security Approach"
+        />
         <div class="carousel-caption d-block">
           <h5 class="c-white">Comprehensive Security Approach</h5>
           <p class="lead">
@@ -142,7 +170,11 @@ export default {
         </div>
       </div>
       <div class="carousel-item">
-        <img :src="home_bg_3" class="d-block min-vh-100" alt="..." />
+        <img
+          :src="home_bg_3"
+          class="d-block min-vh-100"
+          alt="Leadership and Expertise"
+        />
         <div class="carousel-caption d-block">
           <h5 class="c-white">Leadership and Expertise</h5>
           <p class="lead">
@@ -175,3 +207,11 @@ export default {
 
   <Footer />
 </template>
+
+<style scoped>
+.header-img {
+  height: 100%;
+  position: absolute;
+  left: 0;
+}
+</style>
