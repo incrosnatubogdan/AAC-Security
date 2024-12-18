d7398db6056c570fb8e00cbdf837af1e99f1ed11
Incrosnatu Bogdan <incrosnatubogdan@gmail.com>
2024-12-18 18:02:59 +0200

    add img fix

--- vite.config.js
+++ vite.config.js
@@ -5,15 +5,16 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue()],
-    resolve: {
-        alias: {
-            "@": path.resolve(__dirname, "src"),
-            vue: "vue/dist/vue.esm-bundler.js",
-        },
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+      vue: "vue/dist/vue.esm-bundler.js",
     },
-    base: "./",
-    build: {
-        outDir: './docs'
-    }
+  },
+  base: "./",
+  build: {
+    outDir: "./docs",
+    emptyOutDir: false,
+  },
 });
